ed96e671d917e3d45ef2cb0d1e00f18d9e0bbca4
GianlucaA18 <imanol.milla@gmail.com>
2024-11-29 08:50:20 -0500

    common

--- common/Proveedor/EntidadProveedor.cs
+++ common/Proveedor/EntidadProveedor.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Polleria.datos
+namespace Polleria.common.Proveedor
 {
-    internal class EntidadEmpleados
+    internal class EntidadProveedor
     {
     }
 }
